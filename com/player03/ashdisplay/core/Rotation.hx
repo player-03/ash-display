bfc1e421c81b8395747a60c5c1b0f020456754e6
player-03 <playernumberthree@gmail.com>
2015-04-20 19:40:30 -0700

    Default values for convenience.

--- com/player03/ashdisplay/core/Rotation.hx
+++ com/player03/ashdisplay/core/Rotation.hx
@@ -3,7 +3,7 @@ package com.player03.ashdisplay.core;
 class Rotation {
 	public var rotation:Float;
 	
-	public function new(rotation:Float) {
+	public function new(rotation:Float = 0) {
 		this.rotation = rotation;
 	}
 }
--- com/player03/ashdisplay/core/Tile.hx
+++ com/player03/ashdisplay/core/Tile.hx
@@ -3,7 +3,7 @@ package com.player03.ashdisplay.core;
 class Tile {
 	public var id:Int;
 	
-	public function new(id:Int) {
+	public function new(id:Int = 0) {
 		this.id = id;
 	}
 }
